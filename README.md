c87d8485718999f965065f82fe66370e4e1d02b6
Joshua <joshua.wuollet@gmail.com>
2020-02-11 23:27:58 -0600

    Updated Sensors.py to version 2, Updated README

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # AstroPiWeather
 A collection of scripts and plots for the Astro-Pi to collect weather data, and analyze that data in Python 3
 
-Current Version: 1
+######################################################
+Sensors.py
+######################################################
+
+Version 1 of Sensors.py: prints sensor values to file.txt when ran
+
+Version 2 of Sensors.py: Invokes SensorData() every 30 seconds, for 24 hours when ran
+Version 2 has most of the code from V1 now in a function. Plan for version 3 is continuous operation
--- Sensors.py
+++ Sensors.py
@@ -1,14 +1,22 @@
 from sense_hat import SenseHat
 import datetime
+import time
 import subprocess
 sense = SenseHat()
-pressure=sense.get_pressure()
-temp=sense.get_temperature()
-humidity=sense.get_humidity()
-temp_hum=sense.get_temperature_from_humidity()
-temp_press=sense.get_temperature_from_pressure()
-CurrentTime=datetime.datetime.now()
-cpu_temp=float(subprocess.check_output("cat /sys/class/thermal/thermal_zone*/temp", shell=True))/1000
-print([CurrentTime, temp, pressure, temp_press, humidity, temp_hum, cpu_temp])
-with open('file.txt','a') as f:
-    print([CurrentTime, temp, pressure, temp_press, humidity, temp_hum, cpu_temp], file=f)
+
+def SensorData ():
+    time.sleep(30) # Sleep for 30 seconds
+    pressure=sense.get_pressure()
+    temp=sense.get_temperature()
+    humidity=sense.get_humidity()
+    temp_hum=sense.get_temperature_from_humidity()
+    temp_press=sense.get_temperature_from_pressure()
+    CurrentTime=datetime.datetime.now()
+    cpu_temp=float(subprocess.check_output("cat /sys/class/thermal/thermal_zone*/temp", shell=True))/1000
+    #print([CurrentTime, temp, pressure, temp_press, humidity, temp_hum, cpu_temp])
+    with open('file.txt','a') as f:
+        print([CurrentTime, temp, pressure, temp_press, humidity, temp_hum, cpu_temp], file=f)
+    return;
+
+for i in range(2880): # Invoke SensorData() every 30 seconds, for 24 hours
+    SensorData()
